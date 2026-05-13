c5f5ce6cb356e3aed4ef08455d4bc25847788722
Daniel Muthui <dannymuthui118@gmail.com>
2026-05-13 15:28:49 +0300

    feat:linked all routes with

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import MyList from './pages/MyList'
+import ShowDetails from './pages/ShowDetails'
+import {MyListProvider} from './context/MyListContext'
 
-export default function App() {
+function App() {
   return (
-    <div>App</div>
+    <MyListProvider>
+      <Router>
+        <div className='min-h-screen bg-gradient-to-br from-slate-950 via-indigo-950 to-black'>
+        <Navbar />
+        <div className="container mx-auto px-4 py-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/my-list" element={<MyList />} />
+            <Route path="/shows/:id" element={<ShowDetails />} />
+          </Routes>
+        </div>
+        </div>
+      </Router>
+    </MyListProvider>
   )
 }
